161f08c93bbf28581adba7da0680714d7d999b3d
Elvira Sheina <elleuz@gmail.com>
2015-08-15 21:58:39 +0500

    bug fix fixes #11

--- behaviors/AccessBehavior.php
+++ behaviors/AccessBehavior.php
@@ -39,7 +39,7 @@ class AccessBehavior extends AttributeBehavior {
         $this->createRule();
         $user = Instance::ensure(Yii::$app->user, User::className());
         $request = Yii::$app->getRequest();
-        $action = $event->sender->requestedAction;
+        $action = $event->sender->module->requestedAction;
 
         if(!$this->cheсkByRule($action, $user, $request))
         {
